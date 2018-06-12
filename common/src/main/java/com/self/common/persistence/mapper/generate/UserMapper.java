96bfabf0c65df716639cdc5fb9a45edebcb2d897
chengfengfeng <chengzhangheng@gmail.com>
2018-06-12 17:02:53 +0800

    log

---
+++ common/src/main/java/com/self/common/persistence/mapper/generate/UserMapper.java
@@ -0,0 +1,7 @@
+package com.self.common.persistence.mapper.generate;
+
+import com.self.common.persistence.base.BaseMapper;
+import com.self.common.persistence.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
