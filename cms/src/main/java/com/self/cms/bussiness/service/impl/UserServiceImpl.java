8cb8b2b6c77282705faeec8f975318650dbe642b
chengfengfeng <chengzhangheng@gmail.com>
2018-06-12 17:42:14 +0800

    log

--- cms/src/main/java/com/self/cms/bussiness/service/impl/UserServiceImpl.java
+++ cms/src/main/java/com/self/cms/bussiness/service/impl/UserServiceImpl.java
@@ -2,14 +2,23 @@ package com.self.cms.bussiness.service.impl;
 
 
 import com.self.cms.bussiness.service.UserService;
+import com.self.common.persistence.entity.User;
+import com.self.common.persistence.mapper.generate.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- *
  * @author chengfengfeng
  * @date 2018/6/11
  */
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserMapper userMapper;
+
+    @Override
+    public User getUser(int id) {
+        return userMapper.selectByPrimaryKey(id);
+    }
 }
