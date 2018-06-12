9f30bf76aee6573c98f8ad4c17ff416eda6ebb8e
junlintianxia <1373557856@qq.com>
2018-06-12 14:40:12 +0800

    修复找不到mapper的bug

--- cms/src/main/java/com/self/cms/CmsApplication.java
+++ cms/src/main/java/com/self/cms/CmsApplication.java
@@ -3,8 +3,11 @@ package com.self.cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
 @EnableAutoConfiguration
 @SpringBootApplication
+@MapperScan(basePackages = "com.self.common.persistence.mapper")
 public class CmsApplication {
 
     public static void main(String[] args) {
