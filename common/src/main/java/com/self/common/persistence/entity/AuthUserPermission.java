3523352090618c0740b01857f475a69584f4f32b
junlintianxia <1373557856@qq.com>
2018-06-14 10:58:55 +0800

    按用户授予权限

---
+++ common/src/main/java/com/self/common/persistence/entity/AuthUserPermission.java
@@ -0,0 +1,72 @@
+package com.self.common.persistence.entity;
+
+import com.self.common.persistence.base.BaseEntity;
+import javax.persistence.*;
+
+@Table(name = "auth_user_permission")
+public class AuthUserPermission extends BaseEntity {
+    @Id
+    private Integer id;
+
+    /**
+     * 用户ID
+     */
+    @Column(name = "user_id")
+    private Integer userId;
+
+    /**
+     * 权限ID
+     */
+    @Column(name = "permission_id")
+    private Integer permissionId;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取用户ID
+     *
+     * @return user_id - 用户ID
+     */
+    public Integer getUserId() {
+        return userId;
+    }
+
+    /**
+     * 设置用户ID
+     *
+     * @param userId 用户ID
+     */
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * 获取权限ID
+     *
+     * @return permission_id - 权限ID
+     */
+    public Integer getPermissionId() {
+        return permissionId;
+    }
+
+    /**
+     * 设置权限ID
+     *
+     * @param permissionId 权限ID
+     */
+    public void setPermissionId(Integer permissionId) {
+        this.permissionId = permissionId;
+    }
+}
